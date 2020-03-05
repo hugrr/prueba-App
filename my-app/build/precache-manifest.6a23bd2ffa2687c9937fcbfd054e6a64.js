25f0211657971a5de3fd1e39cf1d7fc943ec6813
Hugo Rojas <hurojas.rios@gmail.com>
2020-03-05 20:20:46 +0000

    build

---
+++ my-app/build/precache-manifest.6a23bd2ffa2687c9937fcbfd054e6a64.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22d1a7c031c7c234a6a91346517672c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f8d2d6d200b9ab44a58",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "21dcd1dc28e063284c77",
+    "url": "/static/js/2.4bb9c8bd.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.4bb9c8bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f8d2d6d200b9ab44a58",
+    "url": "/static/js/main.f0921af0.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
